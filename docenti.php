aaacb79cd04edbab5c0bdc0a68629617a789e4d0
Mattia Bondrano <mattcage89@gmail.com>
2015-11-02 23:15:59 +0100

    Primo edit docenti (fino a CC)

--- docenti.php
+++ docenti.php
@@ -55,6 +55,71 @@
                     
                     	<div class="docente">
                     
+                            <img class="foto-docente" src="http://placehold.it/128x128">
+                            
+                            <div class="scheda-docente">
+                            
+                                <h3>Domenica Amoroso</h3>
+                                
+                                <p><b>Animazione 2D</b></p>
+                                
+                                <p>Si laurea in <i>Scienze della Comunicazione</i> presso l’<b>Università di Salerno</b> e continua il suo percorso formativo all’<b>Università La Sapienza di Roma</b> dove consegue il <a href="http://www.masterspettacolo.it/masterspettacolo.html" target="_blank"><i>Master di Animazione 3D Maya e Compositing Apple Shake</i></a>. Dopo il master svolge un tirocinio presso la <b>Fondazione Children Media</b> dove ha la possibilità di seguire le lezioni come tutor e affinare le sue conoscenze. Nel corso degli anni si innamora della grafica 2D mostrando una spiccata propensione alla creazione di loghi e cartoni animati. Si specializza così in grafica vettoriale e realizza numerosi loghi per università, enti di formazione e aziende. Parallelamente lavora come digital artist presso <b>Enjinia</b> realizzando tutte le pin digitali di <i>BeApp</i>, stilizzazioni cartoon di manga, anime e film, e le versioni a cartone animato di personaggi illustri quali gli ospiti di <a href="http://www.teatringestazione.com/altofest/main/" target="_blank"><b>Altofest Napoli</b>, i <i>Negramaro</i>, <i>FaviJ</i> e <i>Cristina D’Avena</i>. Nel 2015 realizza grafica e animazioni della favola interattiva <a href="https://play.google.com/store/books/details/Carlo_Cuomo_Il_Soldo?id=dImmBgAAQBAJ&hl=it" target="_blank">Il Soldo</a> per iOS e Android, specializzandosi nella creazione di questo tipo di prodotto. Attualmente sta lavorando al suo primo cortometraggio animato.</p>
+                            
+                            </div>
+                            
+                            <div class="clearfix"></div>
+                        
+                        </div>
+                    
+                    </div><!-- / Docente -->
+                    
+                    <!-- Docente -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    
+                    	<div class="docente">
+                    
+                            <img class="foto-docente" src="http://placehold.it/256x256">
+                            
+                            <div class="scheda-docente">
+                            
+                                <h3>Enrico Abbate</h3>
+                                                                
+                                <p>Classe 1973, autentico geek con la passione per i videogiochi e l’entusiasmo per la tecnologia. Si avvicina per la prima volta al mondo dell’IT grazie al mitico Commodore 64. <a href="https://www.microsoft.com/learning/en-ie/mct-certification.aspx" target="_blank"><i>Microsoft Certified Trainer</i></a> da oltre 15 anni per i più importanti centri di formazione <b>Microsoft</b> sparsi su tutto il territorio nazionale, ha inoltre maturato esperienza di formazione e consulenza per grandi realtà aziendali e pubbliche amministrazioni, come <b>ASLNAPOLI2NORD</b>, <b>Engineering</b>, <b>Ministero della Difesa</b>, <b>Ministero degli Interni</b>, <b>Luisa Spagnoli</b>, <b>Arma dei Carabinieri</b>, <b>Sogei</b>, <b>Trenitalia</b> ecc. Nel 2010 apre l’azienda <a href="http://www.itpros.it/_/Welcome.html" target="_blank"><b>IT PROS</b></a> che offre servizi e consulenza per il settore IT, formazione, sottotitolazione professionale e traduzione audiovisiva per il cinema e per le aziende in varie combinazioni linguistiche.</p>
+                            
+                            </div>
+                            
+                            <div class="clearfix"></div>
+                        
+                        </div>
+                    
+                    </div><!-- / Docente -->
+                    
+                    <!-- Docente -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    
+                    	<div class="docente">
+                    
+                            <img class="foto-docente" src="http://placehold.it/256x256">
+                            
+                            <div class="scheda-docente">
+                            
+                                <h3>Dario Carella</h3>
+                                                                
+                                <p>Ha conseguito la laurea in <i>Scienze Informatiche</i> presso l’<b>Università degli Studi di Napoli Federico II</b>. Nonostante la sua formazione fosse prevalentemente rivolta allo sviluppo software, negli anni ha maturato una notevole sensibilità per il visual design che in seguito gli ha garantito un accesso come membro attivo della community <a href="https://dribbble.com/" target="_blank"><b>Dribbble</b></a>. A partire dall’ultimo anno universitario ha avuto l’opportunità di collaborare come sviluppatore Android presso la digital agency <a href="http://figmenta.com/" target="_blank"><b>Figmenta</b></a>. Successivamente al completamento del percorso di studi ha iniziato una lunga collaborazione con <b>Enjinia</b>, contribuendo allo sviluppo di svariati progetti sia in ambito web che mobile. Spinto dalla sua passione per la fotografia, ha realizzato <b>Sweerl</b>, piattaforma di condivisione di free stock photos.</p>
+                            
+                            </div>
+                            
+                            <div class="clearfix"></div>
+                        
+                        </div>
+                    
+                    </div><!-- / Docente -->
+                
+                	<!-- Docente -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    
+                    	<div class="docente">
+                    
                             <img class="foto-docente" src="http://placehold.it/256x256">
                             
                             <div class="scheda-docente">
@@ -78,6 +143,29 @@
                     
                     	<div class="docente">
                     
+                            <img class="foto-docente" src="http://placehold.it/256x256">
+                            
+                            <div class="scheda-docente">
+                            
+                                <h3>Michele Citro</h3>
+                                
+                                <p><b>Economia & Marketing</b></p>
+                                                                
+                                <p>Consegue la laurea magistrale in <i>Filosofia Politica e della Comunicazione</i> con specializzazione in <i>Relazioni Pubbliche Internazionali</i>. Ha effettuato ricerche in materia di filosofia politica, linguistica, antropologia e sociologia presso l’<b>Université Paris-Sorbonne</b>, l’<b>EHESS</b> di Parigi e l’<b>Università di Harvard</b> a Boston. Ha lavorato come copywriter per la multinazionale americana di pubblicità <a href="http://www.yr.com/" target="_blank"><b>Young & Rubicam</b></a> e come segretario del <b>Consigliere per la Comunicazione dell’Ambasciata Italiana a Parigi</b>. Attualmente è impegnato in progetti pubblicitari e organizzazione di eventi culturali e artistici oltre ad essere presidente dell’<b>Associazione Culturale Edizioni Paguro</b>. È inoltre autore di due volumi di filosofia politica: <a href="http://www.ibs.it/code/9788854873445/citro-michele/riflettere-storicamente-democrazi.html" target="_blank"><i>Riflettere storicamente la democrazia attraverso Marcel Gauchet</i></a> (Aracne Editrice, 2014) e <a href="http://www.ibs.it/code/9788854885653/citro-michele/libert-a-tra-uomo-e.html" target="_blank"><i>La libertà. Tra uomo e cittadino</i></a> (Aracne Editrice, 2015).</p>
+                            
+                            </div>
+                            
+                            <div class="clearfix"></div>
+                        
+                        </div>
+                    
+                    </div><!-- / Docente -->
+                    
+                    <!-- Docente -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    
+                    	<div class="docente">
+                    
                             <img class="foto-docente" src="http://placehold.it/128x128">
                             
                             <div class="scheda-docente">
@@ -87,6 +175,27 @@
                                 <p><b>Animazione 2D</b><br><b>Character Design & Storyboard</b></p>
                                 
                                 <p>Classe 1980, specializzato in Animazione presso la Scuola Internazionale di Comics di Roma. Durante la sua decennale carriera  ha ricoperto quasi tutte le mansioni e i ruoli che esistono in una produzione di una serie animata, dalla pre-produzione (model pack, storyboard), produzione (supervisone layout, animazione) alla post-produzione (montaggio). Ha confezionato prodotti di animazione vettoriale per Toyota, RAI (Domenica In, Un Medico in Famiglia) e ha lavorato a produzioni animate seriali come Stoneage, Crocco & Dillo, I Saurini 2, La Famiglia Gionni e Capitan Kuk.</p>
+                            
+                            </div>
+                            
+                            <div class="clearfix"></div>
+                        
+                        </div>
+                    
+                    </div><!-- / Docente -->
+                    
+                    <!-- Docente -->
+                    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+                    
+                    	<div class="docente">
+                    
+                            <img class="foto-docente" src="http://placehold.it/128x128">
+                            
+                            <div class="scheda-docente">
+                            
+                                <h3>Fabio Cufari</h3>
+                                                                
+                                <p>Classe 1981. Inizia ad appassionarsi a programmazione e grafica 3D in giovanissima età progettando applicativi e giochi in vari linguaggi di programmazione, sviluppando in proprio diverse esperienze in tutte le fasi di realizzazione di un prodotto. Frequenta la facoltà di Informatica all’<b>Università degli Studi di Napoli Federico II</b> e successivamente lavora in diverse aziende italiane e internazionali come programmatore Java, per poi arrivare a concentrare le sue conoscenze nell’ambito dello sviluppo web e negli aspetti di comunicazione di un prodotto informatico. Lavora per oltre 2 anni presso <a href="http://www.kidea.net/" target="_blank"><b>Kidea</b></a>, agenzia di comunicazione, che gli permette di rafforzare le sue esperienze in diversi rami di sviluppo web e marketing senza mai abbandonare la sua passione per la grafica 3D, conseguendo diversi corsi di specializzazione in modellazione e rendering. Attualmente lavora come freelance e collabora con le startup <b>Enjinia</b> e <a href="http://www.groulies.com/" target="_blank"><b>Groulies</b></a>.</p>
                             
                             </div>
                             
